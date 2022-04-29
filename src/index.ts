229a72d9c808d79084d61f603b6f1ae654b2157d
Yann DUBOIS <yanndubois4@gmail.com>
2022-04-29 14:33:21 +0200

    feat : add body parser support + CRUD ok

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
 import { app } from "./server";
+import mongoose, { ConnectOptions } from "mongoose";
 
-const PORT: string | number = process.env.PORT || 5000;
+const PORT: number | string = process.env.PORT!;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+//TEMPLATE FOR API CALL
+
+// app.listen(PORT, () => {
+//   console.log(`server running on http://localhost:${PORT}`);
+// });
+
+// TEMPLATE FOR DB CONNEXION
+
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.k5xvv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as ConnectOptions;
+
+mongoose
+  .connect(uri, options)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => {
+    throw error;
+  });
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,18 @@ import express, { Express } from "express";
 import cors from "cors";
 import movieRoute from "./routes/movie";
 import { createClient } from "redis";
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
 
 const app: Express = express();
+
+// handle req.body
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 // redis cache init + error + ready
 const redisClient = createClient();
@@ -20,19 +29,5 @@ redisClient.on("ready", () => console.log("Redis Client is ready "));
 
 app.use(cors());
 app.use(movieRoute);
-
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
-
-mongoose
-  .connect(uri, options)
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server running on http://localhost:${PORT}`)
-    )
-  )
-  .catch((error) => {
-    throw error;
-  });
 
 export { app, redisClient };
